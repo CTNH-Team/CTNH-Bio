ba8d788e4bd45a5559f8f780ea695adef19e257a
lucky_block <1774432258@qq.com>
2026-02-05 01:58:19 +0800

    改回gt本体用法

--- src/main/java/com/moguang/ctnhbio/data/recipe/RecipeRemoval.java
+++ src/main/java/com/moguang/ctnhbio/data/recipe/RecipeRemoval.java
@@ -13,23 +13,17 @@ public class RecipeRemoval {
 
     public static void biomancyRemovals(Consumer<ResourceLocation> registry){
         var recipes = new String[]{
-                // 神秘bug
-//                "biomancy:crafting/primordial_cradle",
-//                "biomancy:crafting/primordial_core",
-//                "biomancy:crafting/decomposer",
-//                "biomancy:crafting/despoil_sickle",
-//                "biomancy:crafting/bio_forge",
                 "biofactory:mixing/nutrients_fluid_from_paste",
                 "biofactory:compacting/nutrient_bar_from_fluid",
+                "biomancy:crafting/decomposer",
+                "biomancy:crafting/primordial_cradle",
+                "biomancy:crafting/primordial_core",
+                "biomancy:crafting/despoil_sickle",
+                "biomancy:crafting/bio_forge"
         };
         for(var recipe: recipes) {
-            DataFilterPack.removeRecipe(recipe);
-
+            registry.accept(ResourceLocation.tryParse(recipe));
         }
-//        DataFilterPack.removeRecipeType("biomancy:bio_forging/*");
-//        DataFilterPack.removeRecipeType("biomancy:bio_brewing/*");
-//        DataFilterPack.removeRecipeType("biomancy:digesting/*");
-
         DataFilterPack.removeData("alexsmobs", "^loot_tables/biomancy");
     }
 
