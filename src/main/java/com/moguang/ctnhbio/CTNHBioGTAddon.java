419da79348049de42afdc1ac47f84af8eae9e479
lucky_block <1774432258@qq.com>
2026-07-09 21:54:34 +0800

    删除一些mixin

--- src/main/java/com/moguang/ctnhbio/CTNHBioGTAddon.java
+++ src/main/java/com/moguang/ctnhbio/CTNHBioGTAddon.java
@@ -2,31 +2,20 @@ package com.moguang.ctnhbio;
 
 import com.gregtechceu.gtceu.api.addon.GTAddon;
 import com.gregtechceu.gtceu.api.addon.IGTAddon;
-import com.gregtechceu.gtceu.api.addon.events.KJSRecipeKeyEvent;
-import com.gregtechceu.gtceu.api.recipe.lookup.ingredient.MapIngredientTypeManager;
-import com.gregtechceu.gtceu.integration.kjs.recipe.components.ContentJS;
 
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.resources.ResourceLocation;
 
-import com.moguang.ctnhbio.api.capability.recipe.NutrientRecipeCapability;
 import com.moguang.ctnhbio.api.recipe.ingredient.model.ModelIngredient;
 import com.moguang.ctnhbio.api.recipe.ingredient.model.ModelMapIngredient;
 import com.moguang.ctnhbio.data.CBElements;
 import com.moguang.ctnhbio.data.recipe.RecipeRemoval;
 import com.moguang.ctnhbio.registry.*;
-import com.mojang.datafixers.util.Pair;
-import dev.latvian.mods.kubejs.recipe.component.NumberComponent;
 
 import java.util.function.Consumer;
 
 @GTAddon
 public class CTNHBioGTAddon implements IGTAddon {
-
-    public static final ContentJS<Double> NU_IN = new ContentJS<>(NumberComponent.DOUBLE, NutrientRecipeCapability.CAP,
-            false);
-    public static final ContentJS<Double> NU_OUT = new ContentJS<>(NumberComponent.DOUBLE, NutrientRecipeCapability.CAP,
-            true);
 
     @Override
     public CBRegistrate getRegistrate() {
@@ -37,19 +26,11 @@ public class CTNHBioGTAddon implements IGTAddon {
     public void initializeAddon() {
         CBItems.init();
         CBBlocks.init();
-        MapIngredientTypeManager.registerMapIngredient(ModelIngredient.class, ModelMapIngredient::from);
     }
 
     @Override
     public void registerRecipeCapabilities() {
         CBRecipeCapabilities.init();
-    }
-
-    @Override
-    public void registerRecipeKeys(KJSRecipeKeyEvent event) {
-        event.registerKey(
-                NutrientRecipeCapability.CAP,
-                Pair.of(NU_IN, NU_OUT));
     }
 
     @Override
